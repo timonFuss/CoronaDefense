57b331c60109fd3a8c8bf1a921fe93e24b603357
unknown <m.schmitt92@gmx.net>
2020-06-30 10:42:55 +0200

    changed the order of some layer

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -20,7 +20,7 @@ public class TileScript : MonoBehaviour
 
     private Tower myTower;
 
-    private const int orderOffset = 100;
+    private const int orderOffset = 99;
 
     // Start is called before the first frame update
     void Start()
